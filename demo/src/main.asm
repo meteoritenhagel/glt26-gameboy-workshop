04c222f465869e29c25f921a7cb26aae307245da
Tristan Repolusk <tristan@repolusk.eu>
2026-03-16 22:48:25 +0100

    First commit

---
+++ demo/src/main.asm
@@ -0,0 +1,50 @@
+INCLUDE "./include/hardware.inc"
+
+
+SECTION "Header", ROM0[$100]
+	jp EntryPoint
+	ds $150 - @, 0 ; Make room for the header
+
+EntryPoint:
+	; wait until VBlank to turn the LCD off, otherwise, the display
+	; might take damages
+	call WaitVBlank
+
+	; Turn the LCD off
+    xor a
+    ld [rLCDC], a
+
+	; Copy Tiles
+	ld de, TitleTiles
+    ld hl, $9000
+    ld bc, TitleTilesEnd - TitleTiles
+	call Memcopy
+
+	; Copy Title Tilemap
+	ld de, TitleTilemap
+    ld hl, $9800
+    ld bc, TitleTilemapEnd - TitleTilemap
+	call Memcopy
+
+	; Turn the LCD on
+    ld a, LCDCF_ON | LCDCF_BGON
+    ld [rLCDC], a
+
+	; During the first (blank) frame, initialize display registers
+	ld a, %11100100  ; default palette white:light:dark:black
+	ld [rBGP], a
+
+	ld a, $ff
+.loop
+	jp .loop
+
+
+SECTION "Tiles", ROM0
+TitleTiles::
+INCBIN "./build/title.2bpp"
+TitleTilesEnd::
+
+SECTION "Tilemaps", rom0
+TitleTilemap::
+INCBIN "./build/title.tilemap"
+TitleTilemapEnd::
---
+++ demo/src/utils.asm
@@ -0,0 +1,68 @@
+INCLUDE "./include/hardware.inc"
+INCLUDE "./include/constants.inc"
+
+SECTION "Utility Functions", ROM0
+
+; Copy bytes from one area to another.
+; Invalidates a.
+; @param de: Source
+; @param hl: Destination
+; @param bc: Length
+Memcopy::
+	ld a, [de]
+	ld [hli], a
+	inc de
+	dec bc
+	ld a, b
+	or a, c
+	jp nz, Memcopy
+	ret
+
+; Reads the button inputs.
+; Invalidates a and b.
+; Updates the variables
+;   wCurKeys with the currently pressed keys
+;   wNewKeys with the keys that are now pressed that were not pressed before
+; Taken from https://gbdev.io/gb-asm-tutorial/part2/input.html
+UpdateKeys::
+	; Poll half the controller
+	ld a, P1F_GET_BTN
+	call .onenibble
+	ld b, a ; B7-4 = 1; B3-0 = unpressed buttons
+
+	; Poll the other half
+	ld a, P1F_GET_DPAD
+	call .onenibble
+	swap a ; A3-0 = unpressed directions; A7-4 = 1
+	xor a, b ; A = pressed buttons + directions
+	ld b, a ; B = pressed buttons + directions
+
+	; And release the controller
+	ld a, P1F_GET_NONE
+	ldh [rP1], a
+
+	; Combine with previous wCurKeys to make wNewKeys
+	ld a, [wCurKeys]
+	xor a, b ; A = keys that changed state
+	and a, b ; A = keys that changed to pressed
+	ld [wNewKeys], a
+	ld a, b
+	ld [wCurKeys], a
+	ret
+.onenibble
+	ldh [rP1], a ; switch the key matrix
+	call .knownret ; burn 10 cycles calling a known ret
+	ldh a, [rP1] ; ignore value while waiting for the key matrix to settle
+	ldh a, [rP1]
+	ldh a, [rP1] ; this read counts
+	or a, $F0 ; A7-4 = 1; A3-0 = unpressed keys
+.knownret
+	ret
+
+; Waits until the next VBlank period.
+; Invalidates a.
+WaitVBlank::
+	ld a, [rLY]  ; Copy the vertical line to a
+	cp 144  ; Check if the vertical line is 144
+	jp c, WaitVBlank  ; if yes, we can return, otherwise, wait for longer
+	ret
